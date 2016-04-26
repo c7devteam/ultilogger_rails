2ca44c42698445ffb380c5c486a274f6c51983c0
Matthias Ludwig <matt.ludwig@me.com>
2016-04-26 15:09:34 +0200

    - fixed typo in route path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	namespace :api do
 		namespace :v1 do
 			resources :applications, only: [:index, :show, :create, :update] do
-				resources :request_logs, only: [:index, :show, :create], path: '/requst-logs'
+				resources :request_logs, only: [:index, :show, :create], path: '/reqeust-logs'
 			end
 		end
 	end
