af8fa0a2f3ad237c5d76b061303857cff7f79daa
Matthias Ludwig <matt.ludwig@me.com>
2016-05-03 12:35:37 +0200

    - added db to production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
 set :stage, :production
-server 'ec2-54-194-84-178.eu-west-1.compute.amazonaws.com', user: 'ubuntu', roles: %w{web app}
+server 'ec2-54-194-84-178.eu-west-1.compute.amazonaws.com', user: 'ubuntu', roles: %w{web app db}
